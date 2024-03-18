05c84431b7d603c26e7cb631095297fdf3a97819
Kushagra <skushagra.sharma@gmail.com>
2024-03-18 22:51:13 +0530

    bulksending to 100

---
+++ src/app/api/bulksend/route.ts
@@ -0,0 +1,5 @@
+// for i in $(seq 3); do 
+//   SIGNER="signer-${i}.json"
+//   PUBKEY=$(solana-keygen pubkey "$SIGNER")
+//   spl-token transfer C1iPAct3bbXqpGL6BfYoA5gmwTV71yR3tDcvpZLDm5nq 5000 "$PUBKEY"
+// done
