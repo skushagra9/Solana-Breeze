209fde7904bae97728cab4abc8fbf30238f0625f
Kushagra <skushagra.sharma@gmail.com>
2024-03-21 16:43:00 +0530

    added github

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,28 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useRouter, usePathname } from "next/navigation";
-import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import { SunIcon, MoonIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
 export function Navbar() {
   const { setTheme, theme } = useTheme();
 
   return (
-        <div className="flex flex-row justify-end mx-auto w-full pt-2 md:pt-8 md:px-8 ">
-        
-            <Button
-            variant="ghost"
-            size="icon"
-            onClick={() => {
-                {
-                theme == "light" ? setTheme("dark") : setTheme("light");
-                }
-            }}
-            >
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            </Button>
-        
-        </div>
+    <div className="flex flex-row justify-end mx-auto w-full pt-2 md:pt-8 md:px-8 ">
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => {
+         window.open("https://github.com/skushagra9/Solana-Breeze", "_blank")
+        }}
+      >
+        <GitHubLogoIcon></GitHubLogoIcon>
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => {
+          {
+            theme == "light" ? setTheme("dark") : setTheme("light");
+          }
+        }}
+      >
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </Button>
+    </div>
   );
 }
